fad17e9ae09dbbbc40aa612ede466c48f467922d
jythao <98415818+jythao@users.noreply.github.com>
2022-09-12 11:44:39 -0700

    Create example.cpp

---
+++ example.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main()
+{
+    cout<<"Hi, please enter two whole numbers: ";
+
+    int x,y;
+
+    cin >> x >> y;
+    cout << x + y << endl;
+    cout << x * y << endl;
+    cout << x - y << endl;
+
+    return 0;
+}
